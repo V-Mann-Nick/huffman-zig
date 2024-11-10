eb91d8dd6fb2b1545d90454881c5e818a969528a
Nicklas Sedlock <nicklas.sedlock@posteo.net>
2024-11-10 14:52:23 +0100

    wip

--- node.zig
+++ node.zig
@@ -1,32 +1,63 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
 const LeafNode = struct { char: []const u8 };
 const InternalNode = struct { left: *const Node, right: *const Node };
 const NodeType = enum { leaf, internal };
-const Node = union(NodeType) {
-    leaf: LeafNode,
-    internal: InternalNode,
+const Node = struct {
+    allocator: Allocator,
+    data: union(NodeType) {
+        leaf: LeafNode,
+        internal: InternalNode,
+    },
 
-    fn leaf(char: []const u8) Node {
-        return Node{ .leaf = LeafNode{ .char = char } };
+    fn leaf(allocator: Allocator, char: []const u8) !*const Node {
+        const node = try allocator.create(Node);
+        node.* = Node{
+            .allocator = allocator,
+            .data = .{ .leaf = LeafNode{ .char = char } },
+        };
+        return node;
     }
 
-    fn internal(left: *const Node, right: *const Node) Node {
-        return Node{ .internal = InternalNode{ .left = left, .right = right } };
+    fn internal(allocator: Allocator, left: *const Node, right: *const Node) !*const Node {
+        const node = try allocator.create(Node);
+        node.* = Node{
+            .allocator = allocator,
+            .data = .{ .internal = InternalNode{ .left = left, .right = right } },
+        };
+        return node;
+    }
+
+    fn deinit(node: *const Node) void {
+        switch (node.data) {
+            .internal => |int| {
+                int.left.deinit();
+                int.right.deinit();
+            },
+            .leaf => {},
+        }
+        node.allocator.destroy(node);
     }
 };
 
 test "Node" {
-    const expect = @import("std").testing.expect;
-    const eql = @import("std").mem.eql;
-    const getStdOut = @import("std").io.getStdOut;
+    const expect = std.testing.expect;
+    const eql = std.mem.eql;
+    const getStdOut = std.io.getStdOut;
+    const allocator = std.testing.allocator;
 
     const c1 = "a";
     const c2 = "b";
 
-    const node = Node.internal(&Node.leaf(c1), &Node.leaf(c2));
+    const left = try Node.leaf(allocator, c1);
+    const right = try Node.leaf(allocator, c2);
+    const node = try Node.internal(allocator, left, right);
+    defer node.deinit();
 
     const stdout = getStdOut().writer();
-    try expect(eql(u8, node.internal.left.leaf.char, c1));
+    try expect(eql(u8, node.data.internal.left.data.leaf.char, c1));
     try stdout.print("c1: {X} c2: {X}\n", .{ c1.ptr, c2.ptr });
-    try expect(c1.ptr == node.internal.left.leaf.char.ptr);
-    try expect(eql(u8, node.internal.right.leaf.char, c2));
+    try expect(c1.ptr == node.data.internal.left.data.leaf.char.ptr);
+    try expect(eql(u8, node.data.internal.right.data.leaf.char, c2));
 }
